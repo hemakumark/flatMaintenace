ff7e1db9ae087d8129a825eb3ca93d0afeb95d0a
hemakumark <hemakumar.k@outlook.com>
2015-12-24 15:22:56 +0530

    Deleted unwanted file

--- db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/flats");
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log("Hurray Connected");
-});
